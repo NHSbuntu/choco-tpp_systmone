4efa13a7df82085f782ab0ad46baa305b8e2a4c3
Rob Dyke <robdyke@gmail.com>
2017-05-12 02:38:20 +0100

    Initial choco for TPP Systmone

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,38 +6,47 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName= 'tpp_systmone' # arbitrary name for the package, used in messages
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = '' # download url, HTTPS preferred
+$url        = 'https://portalvhdsd5jq2659yw9p5.blob.core.windows.net/installers/SystmOne.exe' # download url, HTTPS preferred
 $url64      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 #$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
 #$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
 
+$pp = Get-PackageParameters
+
+if ($pp["Edition"] -eq $null -or $pp["Edition"] -eq '') { $pp["Edition"] = 'LicenseKey' }
+
+$silentArgs = "/S /Port:$($pp["Port"]) /Edition:$($pp["Edition"]) /InstallationPath:$($pp["InstallationPath"])"
+if ($pp["AdditionalTools"] -eq 'true') { $silentArgs += " /Additionaltools" }
+
+Write-Debug "This would be the Chocolatey Silent Arguments: $silentArgs"
+
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
+  fileType      = 'EXE' #only one of these: exe, msi, msu
   url           = $url
   url64bit      = $url64
   #file         = $fileLocation
 
-  softwareName  = 'tpp_systmone*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'tpp systmone*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
   # Checksums are now required as of 0.10.0.
-  # To determine checksums, you can get that from the original site if provided. 
-  # You can also use checksum.exe (choco install checksum) and use it 
+  # To determine checksums, you can get that from the original site if provided.
+  # You can also use checksum.exe (choco install checksum) and use it
   # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
+  checksum      = 'd6bba6c0247e8d393b2dcd7f2fe7f5a319288751f2cb5a9f2afb338ff8d457e1'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
   checksum64    = ''
   checksumType64= 'sha256' #default is checksumType
 
   #MSI
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
+  #silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  #validExitCodes= @(0, 3010, 1641)
   #OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
   #silentArgs   = '/S'           # NSIS
   #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
+  silentArgs   =  "/exenoui /q PIDKEY=$($pp["Edition"])"           # InstallShield
   #silentArgs   = '/s /v"/qn"' # InstallShield with MSI
   #silentArgs   = '/s'           # Wise InstallMaster
   #silentArgs   = '-s'           # Squirrel
@@ -51,7 +60,7 @@ $packageArgs = @{
 
 Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
 #Install-ChocolateyZipPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
-## If you are making your own internal packages (organizations), you can embed the installer or 
+## If you are making your own internal packages (organizations), you can embed the installer or
 ## put on internal file share and use the following instead (you'll need to add $file to the above)
 #Install-ChocolateyInstallPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-install-package
 
@@ -96,9 +105,9 @@ Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-ins
 ##Will resolve to C:\Program Files\appname
 #$statementsToRun = "/C `"$appPath\bin\installservice.bat`""
 #Start-ChocolateyProcessAsAdmin $statementsToRun cmd -validExitCodes $validExitCodes
-    
-## add specific folders to the path - any executables found in the chocolatey package 
-## folder will already be on the path. This is used in addition to that or for cases 
+
+## add specific folders to the path - any executables found in the chocolatey package
+## folder will already be on the path. This is used in addition to that or for cases
 ## when a native installer doesn't add things to the path.
 ## - https://chocolatey.org/docs/helpers-install-chocolatey-path
 #Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
@@ -118,7 +127,7 @@ Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-ins
 
 ## Set up a file association
 ## - https://chocolatey.org/docs/helpers-install-chocolatey-file-association
-#Install-ChocolateyFileAssociation 
+#Install-ChocolateyFileAssociation
 
 ## Adding a shim when not automatically found - Cocolatey automatically shims exe files found in package directory.
 ## - https://chocolatey.org/docs/helpers-install-bin-file
